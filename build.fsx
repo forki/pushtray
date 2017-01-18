fa07e9af6c45ea991bbb4ce52d393ff434c45e94
Jatan Patel <jjpatel2@outlook.com>
2017-01-18 16:24:02 -0700

    Add ILRepack to merge assemblies

--- build.fsx
+++ build.fsx
@@ -1,15 +1,16 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.FileUtils
 
 let projectName = "Pushnix"
-let projectFile = sprintf "Pushnix.sln"
+let solution = sprintf "%s.sln" projectName
+let binary = sprintf "%s.exe" <| projectName.ToLower()
 let version = "0.1.0";
-let config = getBuildParamOrDefault "config" "Debug"
+let config = getBuildParamOrDefault "config" "Release"
 let platformTarget = getBuildParamOrDefault "platformTarget" "AnyCPU"
-
 let outputDir = "output"
-let outputDirWithConfig = combinePaths outputDir config
+let outputDirWithConfig = outputDir </> config
 
 Target "Build" (fun _ ->
   let setParams defaults =
@@ -20,6 +21,45 @@ Target "Build" (fun _ ->
           [ "Configuration",   config
             "PlatformTarget",  platformTarget ]
     }
-  build setParams projectFile)
+  build setParams solution)
+
+let execMono cmd =
+  directExec(fun info ->
+    info.FileName <- "mono"
+    info.Arguments <- cmd)
+
+Target "Repack" (fun _ ->
+  let toolPath = "packages" </> "ILRepack" </> "tools" </> "ILRepack.exe"
+  let inputBinary = outputDirWithConfig </> binary
+  let outputMergeDir = outputDirWithConfig </> "merge"
+  let outputBinary = outputMergeDir </> binary
+
+  let libs =
+    [ "FSharp.Core"
+      "FSharp.Data"
+      "websocket-sharp"
+      "BouncyCastle.Crypto"
+      "DocoptNet" ]
+    |> List.map (fun v -> outputDirWithConfig </> (v + ".dll"))
+    |> String.concat " "
+
+  mkdir outputMergeDir
+  sprintf "%s /xmldocs /out:%s %s %s"
+    toolPath
+    outputBinary
+    inputBinary
+    libs
+  |> execMono
+  |> ignore)
+
+Target "Clean" (fun _ ->
+  CleanDir outputDir)
+
+// TODO
+Target "Release" ignore
+
+"Build"
+  ==> "Repack"
+  ==> "Release"
 
 RunTargetOrDefault "Build"
--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/bash
-if [ "$OS" == "Windows_NT" ]; then
-  ./packages/FAKE/tools/FAKE.exe "$@"
-else
-  mono ./packages/FAKE/tools/FAKE.exe "$@"
-fi
+#!/bin/bash
+mono ./packages/FAKE/tools/FAKE.exe "$@"

cd8e027915174b65134e38b4e97231f546f53f4c
Jatan Patel <jjpatel2@outlook.com>
2017-01-19 00:04:28 -0700

    Fix SIGINT being ignored

--- src/app/Pushtray.Core/Program.fs
+++ src/app/Pushtray.Core/Program.fs
@@ -1,3 +1,5 @@
+open Mono.Unix
+open Mono.Unix.Native
 open Gtk
 open Pushtray
 
@@ -7,6 +9,14 @@ let main argv =
 
   Application.Init()
   Pushbullet.connect <| Cli.requiredArg "<encrypt-pass>"
+
+  // Exit gracefully after receiving SIGINT
+  Async.Start(async {
+    if (new UnixSignal(Signum.SIGINT)).WaitOne() then
+      Logger.info "Received SIGINT, exiting..."
+      Application.Quit()
+      exit 1
+  })
 
   // TODO
   TrayIcon.create()

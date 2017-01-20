50ab8b1b3bf173b27d9efa79f1c81da170f048cc
Jatan Patel <jjpatel2@outlook.com>
2017-01-20 01:06:18 -0700

    Fix usage message

--- src/app/Pushtray.Core/Cli.fs
+++ src/app/Pushtray.Core/Cli.fs
@@ -2,17 +2,16 @@ module Pushtray.Cli
 
 open System
 
-let usage = """
+let usage = "\
 usage:
-  pushbullet <access-token> <encrypt-pass> [options]
+  pushtray <access-token> <encrypt-pass> [options]
 
 options:
   --notify-format <fmt>       Select the notification format style (full, short)
   --notify-line-wrap <wrap>   Set the line wrap width (i.e. the maximum width)
   --notify-line-pad <pad>     Set the minimum line width
   --icon-style <style>        Select tray icon style (light, dark)
-  --trace                     Print all log messages
-"""
+  --trace                     Print all log messages"
 
 let private parseArgs (argv: string[]) =
   let docopt = new DocoptNet.Docopt()
